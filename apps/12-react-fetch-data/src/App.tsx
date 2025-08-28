e13f257d4c73caec6eb5e1593ecf0ca5e023206b
pandas-coding <tuanhsiang@163.com>
2025-08-28 23:36:09 +0800

    Added search params to fetch data module.

--- apps/12-react-fetch-data/src/App.tsx
+++ apps/12-react-fetch-data/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { type ChangeEvent, useEffect, useState } from 'react'
 import './App.css'
 import AddNote from './components/AddNote'
 import NoteList from './components/NoteList'
@@ -7,10 +7,16 @@ import SearchNote from './components/SearchNote'
 function App() {
   const [notes, setNotes] = useState([])
   const [loading, setLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
 
-  async function getNotes() {
+  async function getNotes(params?: string) {
     setLoading(true)
-    const res = await fetch('http://localhost:8080/notes')
+
+    let url = 'http://localhost:8080/notes'
+    if (params) {
+      url += `?${new URLSearchParams({ term: params })}`
+    }
+    const res = await fetch(url)
     const data = await res.json()
     setNotes(data)
     setLoading(false)
@@ -20,11 +26,16 @@ function App() {
     getNotes()
   }, [])
 
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+    getNotes(event.target.value)
+  }
+
   return (
     <main className="container">
       <div>
         <h1>我的笔记本</h1>
-        <SearchNote/>
+        <SearchNote searchTerm={searchTerm} onChange={handleSearch}/>
         {loading ? <div>loading...</div> : <NoteList notes={notes}/>}
         <AddNote/>
       </div>
--- apps/12-react-fetch-data/src/components/SearchNote/index.tsx
+++ apps/12-react-fetch-data/src/components/SearchNote/index.tsx
@@ -1,9 +1,20 @@
 import './style.css'
+import type { ChangeEvent } from 'react'
 
-function SearchNote() {
+export interface SearchNoteProps {
+  searchTerm: string;
+  onChange: (ev: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function SearchNote({ searchTerm, onChange }: SearchNoteProps) {
   return (
     <div className="search">
-      <input type="text" placeholder="搜索笔记"/>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={onChange}
+        placeholder="搜索笔记"
+      />
     </div>
   )
 }
