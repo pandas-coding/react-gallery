086e2f49d2bc5e1ec93f0d9b47f905b2d88b8d70
pandas-coding <tuanhsiang@163.com>
2025-08-30 23:10:33 +0800

    Added enhanced react hooks use.

--- apps/13-react-dom/src/App.tsx
+++ apps/13-react-dom/src/App.tsx
@@ -1,43 +1,50 @@
-import "./App.css";
-import VideoPlayer from "./components/VideoPlayer";
+import './App.css'
+import VideoPlayer from './components/VideoPlayer'
 import ForwardRefVideoPlayer from './components/ForwardRefVideoPlayer'
-import { useEffect, useRef, useState } from 'react'
-import Sea from '../../assets/videos/sea.mp4'
-import Play from './play.svg?react'
-import Pause from './pause.svg?react'
+import { useEffect, useId, useRef, useState } from 'react'
+import Sea from './assets/videos/sea.mp4'
+import Play from './assets/play.svg?react'
+import Pause from './assets/pause.svg?react'
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const usernameId = useId()
 
   useEffect(() => {
-    videoRef.current?.play();
-    setIsPlaying(true);
-  }, []);
+    videoRef.current?.play()
+    setIsPlaying(true)
+  }, [])
 
   function handlePlayClick() {
     if (isPlaying) {
-      videoRef.current?.pause();
-      setIsPlaying(false);
+      videoRef.current?.pause()
+      setIsPlaying(false)
     } else {
-      videoRef.current?.play();
-      setIsPlaying(true);
+      videoRef.current?.play()
+      setIsPlaying(true)
     }
   }
 
   return (
     <main className="container">
       <div>
-        <VideoPlayer />
+        <VideoPlayer/>
       </div>
       <div>
         <ForwardRefVideoPlayer src={Sea} ref={videoRef}/>
         <button onClick={handlePlayClick}>
-          {isPlaying ? <Pause /> : <Play />}
+          {isPlaying ? <Pause/> : <Play/>}
         </button>
       </div>
+
+      <form>
+        <label htmlFor={usernameId + '-username'}>用户名</label>
+        <input type="text" id={usernameId + '-username'}/>
+      </form>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
